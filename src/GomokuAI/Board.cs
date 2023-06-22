f3ac7dcdf30468804c14e726007f18d27e42db5d
Cliff <clifford.choi.144@my.csun.edu>
2023-06-21 18:39:55 -0700

    Created board

---
+++ src/GomokuAI/Board.cs
@@ -0,0 +1,26 @@
+﻿namespace GomokuAI;
+
+public class Board
+{
+    private readonly int[,] _board;
+    public int Size { get; }
+
+    public Board(int size)
+    {
+        this.Size = size;
+        _board = new int[size, size];
+    }
+
+    public char GetPosition(int x, int y)
+    {
+        var position = _board[x, y];
+
+        return position switch
+        {
+            0 when x is 3 or 7 or 11 && y is 3 or 7 or 11 => '+',
+            0 => '.',
+            1 => 'X',
+            _ => '0'
+        };
+    }
+}
--- src/GomokuAI/Program.cs
+++ src/GomokuAI/Program.cs
@@ -1,3 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace GomokuAI;
 
-Console.WriteLine("Hello, World!");
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var board = new Board(15);
+
+        Console.WriteLine(new string('_', board.Size * 3 - 1 + 2));
+        
+        for (var i = 0; i < board.Size; i++)
+        {
+            
+            Console.Write("|");
+            
+            for (var j = 0; j < board.Size; j++)
+            {
+                Console.Write(board.GetPosition(i, j) + " ");
+
+                if (j < board.Size - 1)
+                {
+                    Console.Write(" ");
+                }
+            }
+
+            Console.WriteLine("|");
+        }
+        
+        Console.WriteLine(new string('-', board.Size * 3 - 1 + 2));
+        
+    }
+    
+}
