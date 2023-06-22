e543640b938111c39e112fa5f4cb744ad4e0bcce
Cliff <clifford.choi.144@my.csun.edu>
2023-06-21 19:17:30 -0700

    Updated board

--- src/GomokuAI/Board.cs
+++ src/GomokuAI/Board.cs
@@ -13,11 +13,10 @@ public class Board
 
     public char GetPosition(int x, int y)
     {
-        var position = _board[x, y];
+        var position = _board[x-1, y-1];
 
         return position switch
         {
-            0 when x is 3 or 7 or 11 && y is 3 or 7 or 11 => '+',
             0 => '.',
             1 => 'X',
             _ => '0'
--- src/GomokuAI/Program.cs
+++ src/GomokuAI/Program.cs
@@ -5,29 +5,35 @@ public class Program
     public static void Main(string[] args)
     {
         var board = new Board(15);
-
-        Console.WriteLine(new string('_', board.Size * 3 - 1 + 2));
         
-        for (var i = 0; i < board.Size; i++)
+        Console.Write("    ");
+        for (var i = 1; i <= board.Size; i++)
         {
-            
-            Console.Write("|");
-            
-            for (var j = 0; j < board.Size; j++)
+            if (i < 10)
             {
-                Console.Write(board.GetPosition(i, j) + " ");
+                Console.Write(i.ToString().PadLeft(2) + "  ");
+            }
+            else
+            {
+                Console.Write(i.ToString().PadLeft(3) + " ");
+            }
+        }
+        Console.WriteLine();
 
-                if (j < board.Size - 1)
-                {
-                    Console.Write(" ");
-                }
+        for (var i = 1; i <= board.Size; i++)
+        {
+            Console.WriteLine("   " + "+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+");
+            
+            Console.Write(i.ToString().PadLeft(2));
+
+            for (var j = 1; j <= board.Size; j++)
+            {
+                Console.Write(" | " + board.GetPosition(i, j));
             }
 
-            Console.WriteLine("|");
+            Console.WriteLine(" |");
         }
-        
-        Console.WriteLine(new string('-', board.Size * 3 - 1 + 2));
-        
+
+        Console.WriteLine("   " + "+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+");
     }
-    
 }
