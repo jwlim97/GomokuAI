e6f12490d6d8b47a0840d0bc0b7d199948693eed
Cliff <clifford.choi.144@my.csun.edu>
2023-06-25 10:12:03 -0700

    Refactored board logic
    Moved board printing into Board.cs

--- src/GomokuAI/Board.cs
+++ src/GomokuAI/Board.cs
@@ -2,24 +2,72 @@
 
 public class Board
 {
-    private readonly int[,] _board;
-    public int Size { get; }
+    private readonly int _size;
+    private readonly int[,] _positions;
 
     public Board(int size)
     {
-        this.Size = size;
-        _board = new int[size, size];
+        this._size = size;
+        _positions = new int[size, size];
     }
 
-    public char GetPosition(int x, int y)
-    {
-        var position = _board[x-1, y-1];
+    public int Size => _size;
 
-        return position switch
+    public int GetPosition(int row, int column)
+    {
+        return _positions[row - 1, column - 1];
+    }
+
+    public void SetPosition(int row, int column, int value)
+    {
+        _positions[row - 1, column - 1] = value;
+    }
+
+    public void PrintBoard()
+    {
+        // This line ensures that the characters are properly printed. Without this it will print '?'
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        
+        Console.Write("    ");
+        
+        for (var i = 1; i <= _size; i++)
         {
-            0 => '.',
-            1 => 'X',
-            _ => '0'
-        };
+            if (i < 10)
+            {
+                Console.Write(i.ToString().PadLeft(2) + "  ");
+            }
+            else
+            {
+                Console.Write(i.ToString().PadLeft(3) + " ");
+            }
+        }
+        
+        Console.WriteLine();
+
+        for (var i = 1; i <= _size; i++)
+        {
+            Console.WriteLine("   " + "+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+");
+            Console.Write(i.ToString().PadLeft(2));
+
+            for (var j = 1; j <= _size; j++)
+            {
+                var position = GetPosition(i, j);
+
+                switch (position)
+                {
+                    case 1:
+                        Console.Write(" | " + "●");
+                        break;
+                    case 2:
+                        Console.Write(" | " + "○");
+                        break;
+                    default:
+                        Console.Write(" |  ");
+                        break;
+                }
+            }
+            Console.WriteLine(" |");
+        }
+        Console.WriteLine("   " + "+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+");
     }
 }
