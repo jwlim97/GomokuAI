4721a943222d4085231213cde95d45dd24914bbd
Cliff <clifford.choi.144@my.csun.edu>
2023-07-01 14:24:10 -0700

    Implementation of interfaces
    HumanPlayer logic was moved into it's own class
    Basic AI player with random movement implemented

---
+++ src/GomokuAI/Players/AIPlayerEasy.cs
@@ -0,0 +1,27 @@
+﻿using GomokuAI.Engine;
+using GomokuAI.Interfaces;
+
+namespace GomokuAI.Players
+{
+    public class AIPlayerEasy : BaseAIPlayer
+    {
+        public AIPlayerEasy(int playerNumber, Board board) : base(playerNumber, board)
+        {
+        }
+
+        public override (int row, int column) GetMove()
+        {
+            var random = new Random();
+            int row;
+            int column;
+
+            do
+            {
+                row = random.Next(1, Board.Size + 1);
+                column = random.Next(1, Board.Size + 1);
+            } while (_board.GetPosition(row, column) != 0);
+
+            return (row, column);
+        }
+    }
+}
