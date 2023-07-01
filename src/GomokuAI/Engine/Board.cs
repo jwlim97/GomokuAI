678d6efc4875ebcb5a834e879f2a9b1667c61b2c
Cliff <clifford.choi.144@my.csun.edu>
2023-07-01 14:21:32 -0700

    Refactored folder structure
    Created a folder structure
    Game logic is now separated

---
+++ src/GomokuAI/Engine/Board.cs
@@ -0,0 +1,72 @@
+﻿namespace GomokuAI.Engine;
+
+public class Board
+{
+    private const int BoardSize = 15;
+    private readonly int[,] _positions;
+
+    public Board()
+    {
+        _positions = new int[BoardSize, BoardSize];
+    }
+
+    public static int Size => BoardSize;
+
+    public int GetPosition(int row, int column)
+    {
+        return _positions[row - 1, column - 1];
+    }
+
+    public void SetPosition(int row, int column, int value)
+    {
+        _positions[row - 1, column - 1] = value;
+    }
+
+    public void PrintBoard()
+    {
+        // This line ensures that the characters are properly printed. Without this it will print '?'
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        
+        Console.Write("    ");
+        
+        for (var i = 1; i <= BoardSize; i++)
+        {
+            if (i < 10)
+            {
+                Console.Write(i.ToString().PadLeft(2) + "  ");
+            }
+            else
+            {
+                Console.Write(i.ToString().PadLeft(3) + " ");
+            }
+        }
+        
+        Console.WriteLine();
+
+        for (var i = 1; i <= BoardSize; i++)
+        {
+            Console.WriteLine("   " + "+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+");
+            Console.Write(i.ToString().PadLeft(2));
+
+            for (var j = 1; j <= BoardSize; j++)
+            {
+                var position = GetPosition(i, j);
+
+                switch (position)
+                {
+                    case 1:
+                        Console.Write(" | " + "●");
+                        break;
+                    case 2:
+                        Console.Write(" | " + "○");
+                        break;
+                    default:
+                        Console.Write(" |  ");
+                        break;
+                }
+            }
+            Console.WriteLine(" |");
+        }
+        Console.WriteLine("   " + "+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+");
+    }
+}
---
+++ src/GomokuAI/Engine/Gomoku.cs
@@ -0,0 +1,126 @@
+﻿using GomokuAI.Factories;
+using GomokuAI.Interfaces;
+using GomokuAI.Players;
+
+namespace GomokuAI.Engine;
+
+public class Gomoku
+{
+    private readonly Board _board;
+    private readonly IPlayer _player1;
+    private readonly IPlayer _player2;
+    private int _currentPlayer;
+    
+    public Gomoku(int player1Type, int player2Type)
+    {
+        _board = new Board();
+        _player1 = PlayerFactory.CreatePlayer(player1Type, 1, _board);
+        _player2 = PlayerFactory.CreatePlayer(player2Type, 2, _board);
+        _currentPlayer = 1;
+    }
+
+    public void Play()
+    {
+        var isGameFinished = false;
+
+        while (!isGameFinished)
+        {
+            PrintBoard();
+
+            var currentPlayer = GetCurrentPlayer();
+
+            int row, column;
+            if (currentPlayer is BaseAIPlayer)
+            {
+                (row, column) = currentPlayer.GetMove();
+                currentPlayer.PrintMove(row, column);
+            }
+            else
+            {
+                Console.Write($"Player {_currentPlayer}, where would you like to put your piece? (row, column): ");
+                (row, column) = currentPlayer.GetMove();
+            }
+        
+            SetMove(row, column);
+
+            if (!IsGameOver(row, column)) continue;
+            isGameFinished = true;
+            PrintBoard();
+            Console.WriteLine($"Player {_currentPlayer} wins!");
+        }
+    }
+
+
+
+    private IPlayer GetCurrentPlayer()
+    {
+        return _currentPlayer == 1 ? _player1 : _player2;
+    }
+    
+    private void SetMove(int row, int column)
+    {
+        _board.SetPosition(row, column, _currentPlayer);
+
+        if (!IsGameOver(row, column))
+        {
+            SwapPlayer();
+        }
+    }
+
+    private void SwapPlayer()
+    {
+        _currentPlayer = _currentPlayer == 1 ? 2 : 1;
+    }
+
+    private bool CheckDirection(int row, int column, (int rowDirection, int columnDirection) direction)
+    {
+        const int winCount = 5;
+        var count = 0;
+
+        for (var i = -winCount + 1; i < winCount; i++)
+        {
+            var potentialRow = row + i * direction.rowDirection;
+            var potentialColumn = column + i * direction.columnDirection;
+
+            if (potentialRow < 1 || potentialRow > Board.Size || potentialColumn < 1 || potentialColumn > Board.Size)
+            {
+                count = 0;
+                continue;
+            }
+
+            if (_board.GetPosition(potentialRow, potentialColumn) == _currentPlayer)
+            {
+                count++;
+
+                if (count == winCount)
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                count = 0;
+            }
+        }
+
+        return false;
+    }
+    
+    private bool IsGameOver(int row, int column)
+    {
+        var directions = new (int rowDirection, int columnDirection)[]
+        {
+            (1, 0), // Horizontal 
+            (0, 1), // Vertical
+            (1, 1), // Left to right
+            (1, -1) // Right to left
+        };
+
+        return directions.Any(direction => CheckDirection(row, column, direction));
+    }
+    
+    private void PrintBoard()
+    {
+        _board.PrintBoard();
+    }
+}
