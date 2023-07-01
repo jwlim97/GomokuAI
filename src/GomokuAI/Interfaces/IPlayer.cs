d35f4cbe12aae7b9025eef84081e4d1424f519d0
Cliff <clifford.choi.144@my.csun.edu>
2023-07-01 14:23:36 -0700

    Created an interface for players
    User and AI players are both interfaces
    AI players will now implement BaseAIPlayer

---
+++ src/GomokuAI/Interfaces/IPlayer.cs
@@ -0,0 +1,9 @@
+﻿using GomokuAI.Engine;
+
+namespace GomokuAI.Players;
+
+public interface IPlayer
+{
+    public (int row, int column) GetMove();
+    public void PrintMove(int row, int column);
+}
