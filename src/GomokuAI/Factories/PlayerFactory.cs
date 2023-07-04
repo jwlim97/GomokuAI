c3b6944d63ca3bab2dda0ba1558524f0fb94b2eb
Cliff <clifford.choi.144@my.csun.edu>
2023-07-04 15:20:38 -0700

    Added hard AI
    Hard AI is a bit smarter, where it prioritizes it's own win condition rather than blocking
    Near game ending moves, takes anywhere from 1 minute to 130 to compute a move
    Need to figure out some optimizations

--- src/GomokuAI/Factories/PlayerFactory.cs
+++ src/GomokuAI/Factories/PlayerFactory.cs
@@ -13,9 +13,10 @@ public class PlayerFactory
             1 => new HumanPlayer(playerNumber, board),
             2 => new AIPlayerVeryEasy(playerNumber, board),
             3 => new AIPlayerEasy(playerNumber, board),
-            4 => new AIPlayerMedium(playerNumber, board),
+            4 => new AIPlayerMedium(playerNumber, board), 
+            5 => new AIPlayerHard(playerNumber, board),
             // TODO: Place holder for monte carlo
-            // 5 => new AIPlayerHard(playerNumber, board),
+            // 6 => new AIPlayerVeryHard(playerNumber, board),
             _ => throw new Exception($"Wrong player type: {playerType}")
         };
     }
